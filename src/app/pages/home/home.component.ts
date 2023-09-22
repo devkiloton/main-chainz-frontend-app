63187c60f65459595983fb3b5209af632c0cfa8c
devkiloton <dev.kiloton@gmail.com>
2023-09-22 20:36:50 -0300

    Adding chips

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { NgFor } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {MatChipsModule} from '@angular/material/chips';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
@@ -11,6 +12,7 @@ export type Tile = {
   text: string;
   icon: string;
   route: string
+  isActive: boolean | null
 }
 
 
@@ -19,20 +21,22 @@ export type Tile = {
   styleUrls: ['./home.component.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatGridListModule, MatIconModule, NgFor],
+  imports: [MatGridListModule, MatIconModule, MatChipsModule, NgFor],
 })
 export default class HomeComponent {
   private readonly _router = inject(Router);
   public tiles: Array<Tile> = [
-    {text: 'Verificar carteira', cols: 1, rows: 1, color: '#1E559C', icon: 'manage_search', route: 'check-wallet'},
-    {text: 'Extrato de carteira', cols: 1, rows: 1, color: '#1E559C', icon: 'receipt', route: 'check-transaction-history'},
-    {text: 'Criar carteira', cols: 1, rows: 1, color: '#1E559C', icon: 'account_balance_wallet', route: 'create-wallet'},
-    {text: 'Fazer uma transação', cols: 1, rows: 1, color: '#1E559C', icon: 'sync_alt', route: 'create-transfer'},
-    { text: 'Calcular taxa de transação', cols: 1, rows: 1, color: '#1E559C', icon: 'local_gas_station', route: 'check-transaction-fee'},
-    {text: 'Verificar transação', cols: 1, rows: 1, color: '#1E559C', icon: 'done_all', route: 'check-transfer'},
+    {text: 'Verificar carteira', cols: 1, rows: 1, color: '#673ab7', icon: 'manage_search', route: 'check-wallet', isActive: true},
+    {text: 'Extrato de carteira', cols: 1, rows: 1, color: '#673ab7', icon: 'receipt', route: 'check-transaction-history', isActive: true},
+    {text: 'Criar carteira', cols: 1, rows: 1, color: '#673ab7', icon: 'account_balance_wallet', route: 'create-wallet', isActive: null},
+    {text: 'Fazer uma transação', cols: 1, rows: 1, color: '#673ab7', icon: 'sync_alt', route: 'create-transfer', isActive: null},
+    {text: 'Calcular taxa de transação', cols: 1, rows: 1, color: '#673ab7', icon: 'local_gas_station', route: 'check-transaction-fee', isActive: null},
+    {text: 'Verificar transação', cols: 1, rows: 1, color: '#673ab7', icon: 'done_all', route: 'check-transfer', isActive: null},
   ];
 
-  public navigateTo(route: string): void {
-    this._router.navigate([route]);
+  public navigateTo(data: {route: string, isActive: boolean | null}): void {
+    if (data.isActive === true) {
+      this._router.navigate([data.route]);
+    }
   }
 }
