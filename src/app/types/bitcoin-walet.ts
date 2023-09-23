35e3f1e406ada64d0b42a3721bda688ab8c3bffd
devkiloton <dev.kiloton@gmail.com>
2023-09-23 18:47:59 -0300

    Creating bitocin wallet type

---
+++ src/app/types/bitcoin-walet.ts
@@ -0,0 +1,14 @@
+export type BitcoinWallet = {
+    address: string
+    balance: number
+    confirmedBalance: number
+    unconfirmedBalance: number
+    transactionsReference: Array<TransactionsReference>
+  }
+
+  export type TransactionsReference = {
+    transactionId: string
+    confirmations: number
+    value: number
+    blockHeight: number
+  }
