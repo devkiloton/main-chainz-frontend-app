a12c003c551c13114719999bee32588a44e5a96b
devkiloton <dev.kiloton@gmail.com>
2023-09-23 18:48:38 -0300

    Doing request and viewing data 🎉

--- src/app/pages/check-wallet/check-wallet.component.ts
+++ src/app/pages/check-wallet/check-wallet.component.ts
@@ -1,11 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AsyncPipe, JsonPipe, NgFor } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { BehaviorSubject, firstValueFrom } from 'rxjs';
+import { BitcoinApiClientService } from 'src/app/clients/bitcoin-api-client/bitcoin-api-client.service';
+import { allCurrenciesChipOptions } from 'src/app/constants/all-currencies-chip-options';
+import { bitcoinServicesGridTiles } from 'src/app/constants/bitcoin-services-grid-tiles';
+import { ChipIconListComponent } from 'src/app/shared/chip-icon-list/chip-icon-list.component';
+import { GridComponent } from 'src/app/shared/grid/grid.component';
 import { InputTextComponent } from 'src/app/shared/input-text/input-text.component';
+import type { BitcoinWallet } from 'src/app/types/bitcoin-walet';
 
 @Component({
   standalone: true,
-  imports: [InputTextComponent],
+  imports: [InputTextComponent, GridComponent, MatChipsModule, MatIconModule, NgFor, AsyncPipe, JsonPipe, ReactiveFormsModule, ChipIconListComponent],
   templateUrl: './check-wallet.component.html',
   styleUrls: ['./check-wallet.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class CheckWalletComponent {}
+export default class CheckWalletComponent {
+  private readonly _bitcoinApiClientService = inject(BitcoinApiClientService);
+  private readonly _fb = inject(NonNullableFormBuilder)
+
+  public readonly bitcoinServicesGridTiles = bitcoinServicesGridTiles;
+  public readonly allCurrenciesChipOptions = allCurrenciesChipOptions;
+  public readonly walletAddressControl = this._fb.control('', [Validators.required]);
+
+  private readonly _wallet$ = new BehaviorSubject<BitcoinWallet | null>(null);
+  public readonly wallet$ = this._wallet$.asObservable();
+
+  public async checkWallet(): Promise<void> {
+    const data = await firstValueFrom(this._bitcoinApiClientService.wallets.getOneWallet({ id: this.walletAddressControl.value }));
+    this._wallet$.next(data);
+  }
+}
