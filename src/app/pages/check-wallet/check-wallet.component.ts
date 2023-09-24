92766fa8accae41429246af402ca9c24713066ae
devkiloton <dev.kiloton@gmail.com>
2023-09-24 02:11:41 -0300

    Improving types check wallet page

--- src/app/pages/check-wallet/check-wallet.component.ts
+++ src/app/pages/check-wallet/check-wallet.component.ts
@@ -7,6 +7,7 @@ import { BehaviorSubject, firstValueFrom } from 'rxjs';
 import { BitcoinApiClientService } from 'src/app/clients/bitcoin-api-client/bitcoin-api-client.service';
 import { allCurrenciesChipOptions } from 'src/app/constants/all-currencies-chip-options';
 import { bitcoinServicesGridTiles } from 'src/app/constants/bitcoin-services-grid-tiles';
+import type { WalletIdentifiers } from 'src/app/constants/wallet-indentifiers';
 import { ChipIconListComponent } from 'src/app/shared/chip-icon-list/chip-icon-list.component';
 import { GridComponent } from 'src/app/shared/grid/grid.component';
 import { InputTextComponent } from 'src/app/shared/input-text/input-text.component';
@@ -28,11 +29,12 @@ export default class CheckWalletComponent {
   public readonly allCurrenciesChipOptions = allCurrenciesChipOptions;
   public readonly walletAddressControl = this._fb.control('', [Validators.required]);
 
-  private readonly _wallet$ = new BehaviorSubject<BitcoinWallet | null>(null);
+  private readonly _wallet$ = new BehaviorSubject<BitcoinWallet & WalletIdentifiers | null>(null);
   public readonly wallet$ = this._wallet$.asObservable();
 
-  public async checkWallet(): Promise<void> {
+  public async checkBitcoinWallet(): Promise<void> {
     const data = await firstValueFrom(this._bitcoinApiClientService.wallets.getOneWallet({ id: this.walletAddressControl.value }));
-    this._wallet$.next(data);
+    this._wallet$.next({ ...data, code: "BTC", name: "bitcoin" });
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
   }
 }
