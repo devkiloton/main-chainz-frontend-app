135c49b4f68a88dfe8bd2ac89591073aff76d455
devkiloton <dev.kiloton@gmail.com>
2023-09-26 21:23:00 -0300

    Changing logo color according the ttheme

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,14 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ThemesService } from 'src/app/services/themes/themes.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [NgIf, AsyncPipe],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FooterComponent {
+  private readonly _themesService = inject(ThemesService);
+
+  public readonly theme$ = this._themesService.theme$;
 
 }
