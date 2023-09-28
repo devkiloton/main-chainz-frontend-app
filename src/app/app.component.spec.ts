814e33206e9a70e10f3ed04f097dbfeae858f8eb
devkiloton <dev.kiloton@gmail.com>
2023-09-27 23:09:42 -0300

    Adding component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { ToolBarComponent } from './shared/tool-bar/tool-bar.component';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, ToolBarComponent],
+      imports: [RouterTestingModule, ToolBarComponent, FooterComponent],
       declarations: [AppComponent],
     }),
   );
