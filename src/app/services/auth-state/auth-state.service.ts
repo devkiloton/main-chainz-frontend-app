07e0817f03d583d0be5d9394a263ac4d5cac480c
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-08 19:00:58 -0300

    Creating a service to handle the token

---
+++ src/app/services/auth-state/auth-state.service.ts
@@ -0,0 +1,41 @@
+import { inject, Injectable } from '@angular/core';
+import * as jwt_decode from 'jwt-decode';
+import { LocalStorageService } from '../local-storage/local-storage.service';
+import { JWT } from 'src/app/types/jwt';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthStateService {
+  private readonly _localStorage = inject(LocalStorageService);
+
+  public set(value: string): void {
+    this._localStorage.setAuthToken = value;
+  }
+
+  public get(): string | null {
+    return this._localStorage.getAuthToken;
+  }
+
+  public remove(): void {
+    localStorage.removeItem('auth_token');
+  }
+
+  public getDecodedToken(): JWT | null {
+    const token = this.get();
+    if (token !== null) {
+      return jwt_decode.default(token);
+    }
+
+    return null;
+  }
+
+  public isTokenExpired(): boolean {
+    const decodedToken = this.getDecodedToken();
+    if (decodedToken !== null) {
+      return decodedToken.exp < Date.now() / 1000;
+    }
+
+    return true;
+  }
+}
