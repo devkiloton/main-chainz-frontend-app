50be791d2725c311b6c5b608bed334ea67148c29
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-07 23:35:14 -0300

    Making sign  up functional

--- src/app/pages/sign-up/components/sign-up-form/sign-up-form.component.ts
+++ src/app/pages/sign-up/components/sign-up-form/sign-up-form.component.ts
@@ -1,6 +1,17 @@
 import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import type { OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  EventEmitter,
+  forwardRef,
+  inject,
+  Output,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import type { ControlValueAccessor } from '@angular/forms';
+import { NG_VALUE_ACCESSOR, NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -29,13 +40,57 @@ import { InputTextComponent } from 'src/app/shared/input-text/input-text.compone
   templateUrl: './sign-up-form.component.html',
   styleUrls: ['./sign-up-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SignUpFormComponent),
+      multi: true,
+    },
+  ],
 })
-export class SignUpFormComponent {
+export class SignUpFormComponent implements ControlValueAccessor, OnInit {
   private readonly _formBuilder = inject(NonNullableFormBuilder);
+  private readonly _destroyRef = inject(DestroyRef);
+
   public signUpForm = this._formBuilder.group({
     name: [''],
     email: [''],
     password: [''],
     termsAndConditions: [false],
   });
+
+  @Output()
+  public readonly sendEvent = new EventEmitter<{
+    name: string;
+    email: string;
+    password: string;
+    termsAndConditions: boolean;
+  }>();
+
+  public ngOnInit(): void {
+    this.signUpForm.valueChanges.pipe(takeUntilDestroyed(this._destroyRef)).subscribe(() => {
+      const formValue = this.signUpForm.getRawValue();
+      this.writeValue(formValue);
+    });
+  }
+
+  public send(): void {
+    this.sendEvent.emit(this.signUpForm.getRawValue());
+  }
+
+  // ControlValueAccessor Implementation
+  private _onChange = (_value: { name: string; email: string; password: string; termsAndConditions: boolean }): void =>
+    void 0;
+
+  public writeValue(value: { name: string; email: string; password: string; termsAndConditions: boolean }): void {
+    this._onChange(value);
+  }
+
+  public registerOnChange(
+    fn: (_value: { name: string; email: string; password: string; termsAndConditions: boolean }) => void,
+  ): void {
+    this._onChange = fn;
+  }
+
+  public registerOnTouched(_fn: (_value: string) => void): void {}
 }
--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,11 +1,42 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { UserEntity } from 'projects/central-hash-api-client/src/public-api';
 import { SignUpFormComponent } from './components/sign-up-form/sign-up-form.component';
 
 @Component({
   standalone: true,
-  imports: [SignUpFormComponent],
+  imports: [SignUpFormComponent, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SignUpComponent {}
+export default class SignUpComponent {
+  public readonly userEntity = inject(UserEntity);
+  private readonly _formBuilder = inject(NonNullableFormBuilder);
+
+  public readonly createUserInfo = this._formBuilder.control<{
+    name: string;
+    email: string;
+    password: string;
+    termsAndConditions: boolean;
+  }>({
+    name: '',
+    email: '',
+    password: '',
+    termsAndConditions: false,
+  });
+
+  public createUser(data: { name: string; email: string; password: string; termsAndConditions: boolean }): void {
+    const { name, email, password, termsAndConditions } = data;
+    if (termsAndConditions) {
+      this.userEntity.create({ name, email, password }).subscribe({
+        next: value => {
+          console.log(value);
+        },
+        error: error => {
+          console.error(error);
+        },
+      });
+    }
+  }
+}
