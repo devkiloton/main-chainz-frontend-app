e31382e45093772da15a0a0ee5df110c1abd6b7c
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-08 22:51:29 -0300

    Creating sign in constants

---
+++ src/app/constants/sign-in/card-alert.ts
@@ -0,0 +1,7 @@
+export const cardAlert = {
+  requiredEmail: $localize`Type a valid email` as string,
+  requiredPassword: $localize`Type a valid password` as string,
+  invalidData: $localize`Invalid data, try again` as string,
+  problems: $localize`Something went wrong, try again later` as string,
+  generalError: $localize`Fill up all fields` as string,
+};
---
+++ src/app/constants/sign-in/form-field-messages.ts
@@ -0,0 +1,5 @@
+export const formFieldMessages = {
+  requiredPassword: $localize`You must enter a password` as string,
+  requiredEmail: $localize`You must enter an email` as string,
+  invalidEmail: $localize`Not a valid email` as string,
+};
