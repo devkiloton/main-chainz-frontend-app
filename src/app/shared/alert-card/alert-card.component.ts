53243d3eb3b413f0154db0f37551d7e5ecd0707a
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-08 21:14:48 -0300

    Improving error accessibility

--- src/app/shared/alert-card/alert-card.component.ts
+++ src/app/shared/alert-card/alert-card.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import type { OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { UniqueIdService } from 'src/app/services/unique-id/unique-id.service';
 
 @Component({
   selector: 'app-alert-card',
@@ -9,7 +11,15 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrls: ['./alert-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AlertCardComponent {
+export class AlertCardComponent implements OnInit {
+  private readonly _uuidService = inject(UniqueIdService);
+
+  public uuid!: string;
+
   @Input({ required: true })
   public message!: string;
+
+  public ngOnInit(): void {
+    this.uuid = this._uuidService.generateUniqueIdWithPrefix('alert-card');
+  }
 }
