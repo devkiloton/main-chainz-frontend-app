e28866cc143594da793969f856aa7c8395a1a3fb
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-12 21:07:09 -0300

    Testing preview

--- src/app/shared/carousel-prices/carousel-prices.component.ts
+++ src/app/shared/carousel-prices/carousel-prices.component.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { PreviewCurrencyComponent } from '../preview-currency/preview-currency.component';
 
 @Component({
   selector: 'app-carousel-prices',
   standalone: true,
-  imports: [CommonModule],
+  imports: [PreviewCurrencyComponent],
   templateUrl: './carousel-prices.component.html',
   styleUrls: ['./carousel-prices.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
