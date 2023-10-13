b152df7f340bfb7a4c80e44d137379f6dd499b7b
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-13 00:38:59 -0300

    Adding deps APP SPEC

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CarouselPricesComponent } from './shared/carousel-prices/carousel-prices.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ToolBarComponent } from './shared/tool-bar/tool-bar.component';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, ToolBarComponent, FooterComponent],
+      imports: [RouterTestingModule, ToolBarComponent, FooterComponent, CarouselPricesComponent],
       declarations: [AppComponent],
     }),
   );
