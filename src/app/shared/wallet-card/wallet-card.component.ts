4bc26402a14461c5b67833b271af92b87d3ef4e2
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-14 01:34:13 -0300

    Applying changes wallet

--- src/app/shared/wallet-card/wallet-card.component.ts
+++ src/app/shared/wallet-card/wallet-card.component.ts
@@ -1,21 +1,24 @@
-import { CurrencyPipe } from '@angular/common';
+import { AsyncPipe, CurrencyPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { isNil } from 'lodash-es';
+import { firstValueFrom, map } from 'rxjs';
 import type { WalletIdentifiers } from 'src/app/constants/wallet-indentifiers';
-import { numberInverter } from 'src/app/helpers/number-inverter';
 import { AllCurrenciesService } from 'src/app/services/all-currencies/all-currencies.service';
+import { CurrenciesStoreService, FiatCurrenciesStoreService } from 'src/app/stores';
 import type { BitcoinWallet } from 'src/app/types/bitcoin-walet';
 
 @Component({
   selector: 'app-wallet-card',
   standalone: true,
-  imports: [MatCardModule, CurrencyPipe],
+  imports: [MatCardModule, CurrencyPipe, AsyncPipe],
   templateUrl: './wallet-card.component.html',
   styleUrls: ['./wallet-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WalletCardComponent {
+  private readonly _fiatCurrencies = inject(FiatCurrenciesStoreService);
+  private readonly _currency = inject(CurrenciesStoreService);
   private readonly _allCurrenciesService = inject(AllCurrenciesService);
 
   @Input({ required: true }) public wallet!: BitcoinWallet & WalletIdentifiers;
@@ -24,14 +27,15 @@ export class WalletCardComponent {
     return this._allCurrenciesService.getDefaultCurrency;
   }
 
-  public get currencyPrice(): number {
-    const [currencyPrice] = this._allCurrenciesService.getCurrenciesPriceUSD.prices
-      .filter(item => item.code === this.wallet.code.toLowerCase())
-      .map(obj => obj.priceUSD);
+  public async currencyPrice(): Promise<number> {
+    const currencyPrice = await firstValueFrom(
+      this._currency.findOne(this.wallet.code).pipe(map(curr => curr?.price ?? 0)),
+    );
+
     // #TODO add error handling
-    const [fiatPrice] = this._allCurrenciesService.getFiatCurrenciesPriceUSD.prices
-      .filter(item => item.code === this.defaultCurrency.toLowerCase())
-      .map(obj => obj.priceUSD * numberInverter(currencyPrice ?? 0));
+    const fiatPrice = await firstValueFrom(
+      this._fiatCurrencies.findOne(this.defaultCurrency).pipe(map(fiat => (fiat?.rate ?? 0) * currencyPrice)),
+    );
 
     if (!isNil(fiatPrice)) {
       return fiatPrice;
