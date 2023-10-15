44ef0494defddb72d19f4ea0de891a9ddba30987
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-15 15:36:00 -0300

    Removing unnecessary deps

--- src/app/shared/modal-converter/modal-converter.component.ts
+++ src/app/shared/modal-converter/modal-converter.component.ts
@@ -1,15 +1,13 @@
-import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatRippleModule } from '@angular/material/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatMenuModule } from '@angular/material/menu';
 import type { Currency } from 'projects/central-hash-api-client/src/lib/models/currencies/currency';
 import type { FiatCurrency } from 'projects/central-hash-api-client/src/lib/models/fiat-currencies/fiat-currency';
 import { CurrenciesStoreService, FiatCurrenciesStoreService } from 'src/app/stores';
 import { ButtonPrimaryComponent } from '../button-primary/button-primary.component';
+import { CurrencyInputComponent } from './currency-input/currency-input.component';
 
 export type State = {
   flag: string;
@@ -21,17 +19,14 @@ export type State = {
   selector: 'app-modal-converter',
   standalone: true,
   imports: [
-    MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    MatMenuModule,
-    NgFor,
     NgIf,
     FormsModule,
     AsyncPipe,
     ButtonPrimaryComponent,
     ReactiveFormsModule,
-    MatRippleModule,
+    CurrencyInputComponent,
   ],
   templateUrl: './modal-converter.component.html',
   styleUrls: ['./modal-converter.component.scss'],
@@ -40,9 +35,6 @@ export type State = {
 export class ModalConverterComponent {
   private readonly _currenciesStore = inject(CurrenciesStoreService);
   private readonly _fiatCurrenciesStore = inject(FiatCurrenciesStoreService);
-
-  public value = 'Clear me';
-  public options: Array<string> = ['bitcoin', 'ethereum', 'monero', 'theter', 'BNB'];
 
   public get allCurrencies(): Array<Currency> {
     return this._currenciesStore.findAll;
