b4246081fd595309894ba9cd8dd8f6d9ebf37cbe
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-20 22:09:57 -0300

    Updating/adding some new endpoints

--- projects/central-hash-api-client/src/lib/entities/auth/auth-entity.service.ts
+++ projects/central-hash-api-client/src/lib/entities/auth/auth-entity.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { Auth as AuthModel } from '../../models/auth/auth';
 import { Observable } from 'rxjs';
+import { SignIn } from '../../models/auth/sign-in';
+import { Auth } from '../../models/auth/auth';
+import { SignUp } from '../../models/auth/sign-up';
 import { Response } from '../../shared/response';
 
 @Injectable({
@@ -12,7 +14,23 @@ export class AuthEntity {
   private readonly API = environment.centralHashApiUrl;
   private readonly _httpClient = inject(HttpClient);
 
-  public signin(data: AuthModel): Observable<Response<{ access_token: string }>> {
-    return this._httpClient.post<Response<{ access_token: string }>>(`${this.API}/auth/sign-in`, data);
+  public signIn(data: SignIn): Observable<Auth> {
+    return this._httpClient.post<Auth>(`${this.API}/auth/sign-in`, data);
+  }
+
+  public signUp(data: SignUp): Observable<Auth> {
+    return this._httpClient.post<Auth>(`${this.API}/auth/sign-up`, data);
+  }
+
+  public emitCode(data: { email: string }): Observable<Response<{ message: string }>> {
+    return this._httpClient.post<Response<{ message: string }>>(`${this.API}/auth/emit-code`, data);
+  }
+
+  public resetPassword(data: {
+    email: string;
+    code: string;
+    newPassword: string;
+  }): Observable<Response<{ message: string }>> {
+    return this._httpClient.patch<Response<{ message: string }>>(`${this.API}/auth/reset-password`, data);
   }
 }
