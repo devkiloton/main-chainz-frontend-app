ed6a596f60457a88bc8c62b88af61d44192c3ae2
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-20 21:42:48 -0300

    Reaming props and implementing constants

--- src/app/shared/dialog-change-password/dialog-change-password.component.ts
+++ src/app/shared/dialog-change-password/dialog-change-password.component.ts
@@ -1,3 +1,4 @@
+import { NgFor } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,6 +7,7 @@ import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatStepperModule } from '@angular/material/stepper';
+import { dialogueSteps } from 'src/app/constants/sign-in/forgot-password-dialogue-steps';
 import { InputTextComponent } from '../input-text/input-text.component';
 
 @Component({
@@ -21,6 +23,7 @@ import { InputTextComponent } from '../input-text/input-text.component';
     InputTextComponent,
     MatFormFieldModule,
     MatInputModule,
+    NgFor,
   ],
   templateUrl: './dialog-change-password.component.html',
   styleUrls: ['./dialog-change-password.component.scss'],
@@ -29,13 +32,53 @@ import { InputTextComponent } from '../input-text/input-text.component';
 export class DialogChangePasswordComponent {
   private readonly _fb = inject(NonNullableFormBuilder);
   public isLinear = true;
-  public firstFormGroup = this._fb.group({
-    firstCtrl: ['', Validators.required],
+  public emailFormGroup = this._fb.group({
+    email: ['', Validators.required],
   });
 
-  public secondFormGroup = this._fb.group({
-    secondCtrl: ['', Validators.required],
+  public codeFormGroup = this._fb.group({
+    code: ['', Validators.required],
   });
+
+  public newPasswordFormGroup = this._fb.group({
+    newPassword: ['', Validators.required],
+  });
+
+  public readonly steps = [
+    {
+      formGroup: this.emailFormGroup,
+      formControl: this.emailFormGroup.controls.email,
+      header: dialogueSteps.email.header,
+      description: dialogueSteps.email.description,
+      secondaryButtonText: dialogueSteps.email.secondaryButtonText,
+      primaryButtonText: dialogueSteps.email.primaryButtonText,
+      label: dialogueSteps.email.label,
+      placeholder: dialogueSteps.email.placeholder,
+      type: dialogueSteps.email.type,
+    },
+    {
+      formGroup: this.codeFormGroup,
+      formControl: this.codeFormGroup.controls.code,
+      header: dialogueSteps.code.header,
+      description: dialogueSteps.code.description,
+      secondaryButtonText: dialogueSteps.code.secondaryButtonText,
+      primaryButtonText: dialogueSteps.code.primaryButtonText,
+      label: dialogueSteps.code.label,
+      placeholder: dialogueSteps.code.placeholder,
+      type: dialogueSteps.code.type,
+    },
+    {
+      formGroup: this.newPasswordFormGroup,
+      formControl: this.newPasswordFormGroup.controls.newPassword,
+      header: dialogueSteps.newPassword.header,
+      description: dialogueSteps.newPassword.description,
+      secondaryButtonText: dialogueSteps.newPassword.secondaryButtonText,
+      primaryButtonText: dialogueSteps.newPassword.primaryButtonText,
+      label: dialogueSteps.newPassword.label,
+      placeholder: dialogueSteps.newPassword.placeholder,
+      type: dialogueSteps.newPassword.type,
+    },
+  ];
 
   constructor(public dialogRef: MatDialogRef<DialogChangePasswordComponent>) {}
 }
