9bfc0eaeede6d1cf792b9b930877078c67b230a4
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-22 02:56:54 -0300

    Improving the api client

--- projects/central-hash-api-client/src/lib/entities/auth/auth-entity.service.ts
+++ projects/central-hash-api-client/src/lib/entities/auth/auth-entity.service.ts
@@ -14,23 +14,39 @@ export class AuthEntity {
   private readonly API = environment.centralHashApiUrl;
   private readonly _httpClient = inject(HttpClient);
 
-  public signIn(data: SignIn): Observable<Auth> {
-    return this._httpClient.post<Auth>(`${this.API}/auth/sign-in`, data);
+  public signIn(data: SignIn): Observable<{ message: 'Confirmation email sent' }> {
+    return this._httpClient.post<{ message: 'Confirmation email sent' }>(`${this.API}/auth/sign-in`, data);
   }
 
-  public signUp(data: SignUp): Observable<Auth> {
-    return this._httpClient.post<Auth>(`${this.API}/auth/sign-up`, data);
+  public signUp(data: SignUp): Observable<{ message: 'Confirmation email sent' }> {
+    return this._httpClient.post<{ message: 'Confirmation email sent' }>(`${this.API}/auth/sign-up`, data);
   }
 
-  public emitCode(data: { email: string }): Observable<Response<{ message: string }>> {
-    return this._httpClient.post<Response<{ message: string }>>(`${this.API}/auth/emit-code`, data);
+  public signOut(): Observable<Response<{ message: string }>> {
+    return this._httpClient.post<Response<{ message: string }>>(`${this.API}/auth/sign-out`, {});
+  }
+
+  public requestResetPassword(data: { email: string }): Observable<Response<{ message: string }>> {
+    return this._httpClient.post<Response<{ message: string }>>(`${this.API}/auth/request-reset-password`, data);
   }
 
   public resetPassword(data: {
     email: string;
     code: string;
-    newPassword: string;
+    password: string;
   }): Observable<Response<{ message: string }>> {
     return this._httpClient.patch<Response<{ message: string }>>(`${this.API}/auth/reset-password`, data);
+  }
+
+  public verifySignGeneral(data: { email: string; code: string }): Observable<Auth> {
+    return this._httpClient.post<Auth>(`${this.API}/auth/verify-sign-general`, data);
+  }
+
+  public verifyResetPassword(data: { email: string; code: string }): Observable<Response<{ message: string }>> {
+    return this._httpClient.post<Response<{ message: string }>>(`${this.API}/auth/verify-reset-password`, data);
+  }
+
+  public refresh(): Observable<Auth> {
+    return this._httpClient.get<Auth>(`${this.API}/auth/refresh`, {});
   }
 }
