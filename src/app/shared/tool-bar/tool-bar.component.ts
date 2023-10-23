f238cdacdaa12c4c39e0733ade8392210d67f82f
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-22 21:41:12 -0300

    Removing unnecessary modules

--- src/app/shared/tool-bar/tool-bar.component.ts
+++ src/app/shared/tool-bar/tool-bar.component.ts
@@ -1,25 +1,19 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { AsyncPipe, DOCUMENT, NgIf } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Inject, inject } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, inject, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatChipsModule } from '@angular/material/chips';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { Router, RouterModule } from '@angular/router';
-import type { Currency } from 'projects/central-hash-api-client/src/lib/models/currencies/currency';
-import type { FiatCurrency } from 'projects/central-hash-api-client/src/lib/models/fiat-currencies/fiat-currency';
+import { RouterModule } from '@angular/router';
 import { BehaviorSubject, map } from 'rxjs';
 import { AccessiblePressDirective } from 'src/app/directives/accessible-press.directive';
 import { FiatCurrencies } from 'src/app/enums/fiat-currencies';
-import { AllCurrenciesService } from 'src/app/services/all-currencies/all-currencies.service';
 import { ThemesService } from 'src/app/services/themes/themes.service';
-import { FiatCurrenciesStoreService } from 'src/app/stores';
-import { isFiatCurrency } from 'src/app/type-guards/is-fiat-currency';
-import { environment } from 'src/environments/environment';
-import { CurrenciesMenuComponent } from '../currencies-menu/currencies-menu.component';
+import { ButtonChangeCurrencyComponent } from '../button-change-currency/button-change-currency.component';
+import { ButtonChangeThemeComponent } from '../button-change-theme/button-change-theme.component';
+import { ChangeLocaleButtonComponent } from '../change-locale-button/change-locale-button.component';
 import { TreeComponent } from './components/tree/tree.component';
 
 @Component({
@@ -30,28 +24,24 @@ import { TreeComponent } from './components/tree/tree.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatButtonModule,
-    MatChipsModule,
     MatMenuModule,
-    MatSidenavModule,
     MatDividerModule,
     RouterModule,
     AccessiblePressDirective,
     NgIf,
     AsyncPipe,
-    CurrenciesMenuComponent,
     TreeComponent,
+    ButtonChangeThemeComponent,
+    ButtonChangeCurrencyComponent,
+    ChangeLocaleButtonComponent,
   ],
   templateUrl: './tool-bar.component.html',
   styleUrls: ['./tool-bar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ToolBarComponent {
-  private readonly _router = inject(Router);
-  private readonly _allCurrenciesService = inject(AllCurrenciesService);
   private readonly _themesService = inject(ThemesService);
   private readonly _breakpointObserver = inject(BreakpointObserver);
-  private readonly _fiatCurrenciesStore = inject(FiatCurrenciesStoreService);
 
   public showFiller = false;
   public readonly fiatCurrencies = FiatCurrencies;
@@ -63,35 +53,10 @@ export class ToolBarComponent {
     .observe('(max-width: 1024px)')
     .pipe(map(value => !value.matches));
 
-  constructor(@Inject(DOCUMENT) private readonly _document: Document) {}
+  @Output()
+  public readonly sideNavToggle = new EventEmitter<void>();
 
-  public changeLocaleServer(locale: string): void {
-    this._document.location.href = `${environment.appHostUrl}/${locale}${this._router.routerState.snapshot.url}`;
-  }
-
-  public changeDefaultCurrency(currency: FiatCurrency | Currency): void {
-    if (isFiatCurrency(currency)) {
-      this._allCurrenciesService.setDefaultCurrency = currency;
-    }
-  }
-
-  public changeTheme(theme: 'dark-mode' | 'light-mode'): void {
-    this._themesService.setTheme = theme;
-  }
-
-  public changeDrowerState(): void {
-    if (!this._drawerState$.value) {
-      this._drawerState$.next(!this._drawerState$.value);
-      return;
-    }
-
-    // #TODO: Remove this timeout when the bug is fixed
-    setTimeout(() => {
-      this._drawerState$.next(!this._drawerState$.value);
-    }, 150);
-  }
-
-  public get allFiatCurrencies(): Array<FiatCurrency> {
-    return this._fiatCurrenciesStore.findAll;
+  public toggleSideNav(): void {
+    this.sideNavToggle.emit();
   }
 }
