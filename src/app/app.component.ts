d423663601fe718d7e4cdd4cd0e164add3d021a7
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-22 21:38:21 -0300

    Adding personalized svg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { isPlatformBrowser } from '@angular/common';
 import type { OnInit } from '@angular/core';
 import { Component, Inject, inject, PLATFORM_ID } from '@angular/core';
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import { MatIconRegistry } from '@angular/material/icon';
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import { DomSanitizer } from '@angular/platform-browser';
 import { LocalStorageService } from './services/local-storage/local-storage.service';
 import { ThemesService } from './services/themes/themes.service';
 
@@ -12,7 +16,16 @@ import { ThemesService } from './services/themes/themes.service';
 export class AppComponent implements OnInit {
   private readonly _localStorageService = inject(LocalStorageService);
   private readonly _themesService = inject(ThemesService);
-  constructor(@Inject(PLATFORM_ID) private readonly _platformId: object) {}
+  constructor(
+    @Inject(PLATFORM_ID) private readonly _platformId: object,
+    private readonly _matIconRegistry: MatIconRegistry,
+    private readonly _domSanitizer: DomSanitizer,
+  ) {
+    this._matIconRegistry.addSvgIcon(
+      'decentralized_protocols',
+      this._domSanitizer.bypassSecurityTrustResourceUrl('../assets/images/decentralized-protocols.svg'),
+    );
+  }
 
   public async ngOnInit(): Promise<void> {
     if (isPlatformBrowser(this._platformId)) {
