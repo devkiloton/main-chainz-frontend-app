9774cd3e77b794d63854169b9b5ed9e70255c0ff
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-22 22:48:56 -0300

    npm run format

--- src/app/shared/button-change-theme/button-change-theme.component.spec.ts
+++ src/app/shared/button-change-theme/button-change-theme.component.spec.ts
@@ -1,14 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import type { ComponentFixture } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { ButtonChangeThemeComponent } from './button-change-theme.component';
 
 describe('ButtonChangeThemeComponent', () => {
-  let component: ButtonChangeThemeComponent;
-  let fixture: ComponentFixture<ButtonChangeThemeComponent>;
+  let component: ButtonChangeThemeComponent | null = null;
+  let fixture: ComponentFixture<ButtonChangeThemeComponent> | null = null;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ButtonChangeThemeComponent]
+      imports: [ButtonChangeThemeComponent],
     });
     fixture = TestBed.createComponent(ButtonChangeThemeComponent);
     component = fixture.componentInstance;
--- src/app/shared/change-locale-button/change-locale-button.component.spec.ts
+++ src/app/shared/change-locale-button/change-locale-button.component.spec.ts
@@ -1,14 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import type { ComponentFixture } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { ChangeLocaleButtonComponent } from './change-locale-button.component';
 
 describe('ChangeLocaleButtonComponent', () => {
-  let component: ChangeLocaleButtonComponent;
-  let fixture: ComponentFixture<ChangeLocaleButtonComponent>;
+  let component: ChangeLocaleButtonComponent | null = null;
+  let fixture: ComponentFixture<ChangeLocaleButtonComponent> | null = null;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ChangeLocaleButtonComponent]
+      imports: [ChangeLocaleButtonComponent],
     });
     fixture = TestBed.createComponent(ChangeLocaleButtonComponent);
     component = fixture.componentInstance;
