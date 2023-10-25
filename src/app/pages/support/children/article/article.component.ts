1bd7d672e94a36de0ea7a48050faee0c618c934d
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-24 22:38:15 -0300

    Rendering markdown in the front

--- src/app/pages/support/children/article/article.component.ts
+++ src/app/pages/support/children/article/article.component.ts
@@ -1,10 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [MarkdownModule, AsyncPipe],
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export default class ArticleComponent {}
