a3d8b42b03ffe071ad071034fb6eba07fb259b80
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-26 01:10:41 -0300

    Making header buttons functional

--- src/app/pages/support/components/header/header.component.ts
+++ src/app/pages/support/components/header/header.component.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { InputTextComponent } from 'src/app/shared/input-text/input-text.component';
-import { CardQuestionsComponent } from '../card-questions/card-questions.component';
+import { CardCategoryComponent } from '../card-category/card-category.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [InputTextComponent, CardQuestionsComponent],
+  imports: [InputTextComponent, CardCategoryComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
