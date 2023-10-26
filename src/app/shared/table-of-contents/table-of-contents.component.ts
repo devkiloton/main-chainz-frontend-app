26f021ada00dee2c4c4190eb56844c648b71fb60
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-26 01:08:53 -0300

    Making table of contents functional

--- src/app/shared/table-of-contents/table-of-contents.component.ts
+++ src/app/shared/table-of-contents/table-of-contents.component.ts
@@ -1,9 +1,14 @@
+import { NgFor } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import type { CategorySupport } from 'src/app/types/support/category-support';
 
 @Component({
   selector: 'app-table-of-contents',
   standalone: true,
-  imports: [],
+  imports: [NgFor, RouterModule, MatIconModule, MatButtonModule],
   templateUrl: './table-of-contents.component.html',
   styleUrls: ['./table-of-contents.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -11,4 +16,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 export class TableOfContentsComponent {
   @Input({ required: true })
   public headerText!: string;
+
+  @Input({ required: true })
+  public categories!: Array<CategorySupport>;
 }
