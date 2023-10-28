be9f2694fa5c7c621ca8810a885dc22fafdccc3a
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-28 20:48:32 -0300

    Removing unnecessary component

--- src/app/pages/support/components/header/header.component.ts
+++ src/app/pages/support/components/header/header.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { InputTextComponent } from 'src/app/shared/input-text/input-text.component';
-import { CardCategoryComponent } from '../card-category/card-category.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [InputTextComponent, CardCategoryComponent],
+  imports: [InputTextComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
