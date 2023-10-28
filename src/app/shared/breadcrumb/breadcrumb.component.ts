21ee052192f581d43fb2fa04eb9496c77a5e4088
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-28 14:33:13 -0300

    Making breadcrumb reactive

--- src/app/shared/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -1,6 +1,7 @@
 import { AsyncPipe, NgFor } from '@angular/common';
 import type { OnInit } from '@angular/core';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 
@@ -14,20 +15,23 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class BreadcrumbComponent implements OnInit {
   private readonly _routeSnapshot = inject(ActivatedRoute);
+  private readonly _destroyRef = inject(DestroyRef);
 
   private readonly _breadcrumbPaths$ = new BehaviorSubject<Array<{ path: string; name: string }>>([]);
   public readonly breadcrumbPaths$ = this._breadcrumbPaths$.asObservable();
 
   public async ngOnInit(): Promise<void> {
-    const pathFromRoot = this._routeSnapshot.pathFromRoot
-      .map(v => v.snapshot.url.map(v => v.path).join('/'))
-      .join('/')
-      .split('/');
-    const breadcrumbPaths = pathFromRoot.map((v, i) => {
-      const path = pathFromRoot.slice(0, i + 1).join('/');
-      const name = v.replace(/_/g, ' ').replace(/\b\w/g, l => l.toUpperCase());
-      return { path, name: name === '' ? 'Home' : name };
+    this._routeSnapshot.url.pipe(takeUntilDestroyed(this._destroyRef)).subscribe(() => {
+      const pathFromRoot = this._routeSnapshot.pathFromRoot
+        .map(v => v.snapshot.url.map(v => v.path).join('/'))
+        .join('/')
+        .split('/');
+      const breadcrumbPaths = pathFromRoot.map((v, i) => {
+        const path = pathFromRoot.slice(0, i + 1).join('/');
+        const name = v.replace(/_/g, ' ').replace(/\b\w/g, l => l.toUpperCase());
+        return { path, name: name === '' ? 'Home' : name };
+      });
+      this._breadcrumbPaths$.next(breadcrumbPaths);
     });
-    this._breadcrumbPaths$.next(breadcrumbPaths);
   }
 }
