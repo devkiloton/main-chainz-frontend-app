18b7a3c0f8b2fdb4517a75e5457be4bce7f6bd7e
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-28 12:36:34 -0300

    Implementing most viewed section

--- src/app/pages/support/support.component.ts
+++ src/app/pages/support/support.component.ts
@@ -1,11 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AsyncPipe, NgFor } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ArticlesService } from 'projects/central-hash-api-client/src/public-api';
+import { CardQuestionsComponent } from './components/card-questions/card-questions.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CardQuestionsComponent, NgFor, AsyncPipe],
   templateUrl: './support.component.html',
   styleUrls: ['./support.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SupportComponent {}
+export default class SupportComponent {
+  private readonly _articlesService = inject(ArticlesService);
+
+  public readonly articles$ = this._articlesService.findMostViewed();
+}
