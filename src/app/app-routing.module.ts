9754cb881df8c2ebced8e9c14d80a9ba7e972ffe
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-29 17:45:11 -0300

    Rebranding the name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,59 +5,59 @@ import { authorizationGuard } from './guards/authorization-guard/authorization.g
 
 const routes: Routes = [
   {
-    title: $localize`Central Hash` as string,
+    title: $localize`Main Chainz` as string,
     path: '',
     pathMatch: 'full',
     redirectTo: 'home',
   },
   {
-    title: $localize`Central Hash | Home` as string,
+    title: $localize`Main Chainz | Home` as string,
     path: 'home',
     pathMatch: 'full',
     loadComponent: () => import('./pages/home/home.component'),
   },
   {
-    title: $localize`Central Hash | Support` as string,
+    title: $localize`Main Chainz | Support` as string,
     path: 'support',
     children: [
       {
-        title: $localize`Central Hash | Support` as string,
+        title: $localize`Main Chainz | Support` as string,
         path: '',
         loadComponent: () => import('./pages/support/support.component'),
       },
       {
-        title: $localize`Central Hash | Support` as string,
+        title: $localize`Main Chainz | Support` as string,
         path: ':category',
         loadComponent: () => import('./pages/support/children/category/category.component'),
       },
       {
-        title: $localize`Central Hash | Support` as string,
+        title: $localize`Main Chainz | Support` as string,
         path: ':category/:article',
         loadComponent: () => import('./pages/support/children/article/article.component'),
       },
     ],
   },
   {
-    title: $localize`Central Hash | Dashboard` as string,
+    title: $localize`Main Chainz | Dashboard` as string,
     path: 'dashboard',
     pathMatch: 'full',
     canActivate: [authorizationGuard],
     loadComponent: () => import('./pages/dashboard/dashboard.component'),
   },
   {
-    title: $localize`Central Hash | Sign in` as string,
+    title: $localize`Main Chainz | Sign in` as string,
     path: 'sign-in',
     pathMatch: 'full',
     loadComponent: () => import('./pages/sign-in/sign-in.component'),
   },
   {
-    title: $localize`Central Hash | Sign up` as string,
+    title: $localize`Main Chainz | Sign up` as string,
     path: 'sign-up',
     pathMatch: 'full',
     loadComponent: () => import('./pages/sign-up/sign-up.component'),
   },
   {
-    title: $localize`Central Hash | Not found` as string,
+    title: $localize`Main Chainz | Not found` as string,
     path: 'not-found',
     pathMatch: 'full',
     loadComponent: () => import('./pages/not-found/not-found.component'),
