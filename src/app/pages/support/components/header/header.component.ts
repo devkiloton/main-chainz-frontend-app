3d3b08aed8dfed58bcb771255bb57037f331ab85
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-29 17:53:43 -0300

    Improving styles

--- src/app/pages/support/components/header/header.component.ts
+++ src/app/pages/support/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ShapeBehindDirective } from 'src/app/directives/shape-behind/shape-behind.directive';
 import { InputTextComponent } from 'src/app/shared/input-text/input-text.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [InputTextComponent],
+  imports: [InputTextComponent, ShapeBehindDirective],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
