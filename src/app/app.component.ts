ce80b04f0982db4b9b089096c2c064750b872100
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-29 19:31:18 -0300

    Creating fn and finishing PR lol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { isPlatformBrowser } from '@angular/common';
 import type { OnInit } from '@angular/core';
-import { Component, Inject, inject, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, inject, LOCALE_ID, PLATFORM_ID } from '@angular/core';
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
 import { MatIconRegistry } from '@angular/material/icon';
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
 import { DomSanitizer } from '@angular/platform-browser';
+import type { Locales } from './enums/locales';
+import { getFiatAccordingLocale } from './helpers/get-fiat-according-locale';
 import { LocalStorageService } from './services/local-storage/local-storage.service';
 import { ThemesService } from './services/themes/themes.service';
 
@@ -18,12 +20,14 @@ export class AppComponent implements OnInit {
   private readonly _themesService = inject(ThemesService);
   constructor(
     @Inject(PLATFORM_ID) private readonly _platformId: object,
+    @Inject(LOCALE_ID) private readonly _localeId: Locales,
     private readonly _matIconRegistry: MatIconRegistry,
     private readonly _domSanitizer: DomSanitizer,
   ) {}
 
   public async ngOnInit(): Promise<void> {
     if (isPlatformBrowser(this._platformId)) {
+      this._localStorageService.setDefaultCurrency = getFiatAccordingLocale(this._localeId);
       this._themesService.setTheme = this._localStorageService.getDefaultTheme;
     }
     this._matIconRegistry.addSvgIcon(
---
+++ src/app/helpers/get-fiat-according-locale.ts
@@ -0,0 +1,17 @@
+import { FiatCurrencies } from '../enums/fiat-currencies';
+import { Locales } from '../enums/locales';
+
+/**
+ * Sets the fiat currency according to the locale.
+ * @param locale - A {@link Locales} enum value.
+ */
+export const getFiatAccordingLocale = (locale: Locales): FiatCurrencies => {
+  switch (locale) {
+    case Locales.en_us:
+      return FiatCurrencies.usd;
+    case Locales.pt_br:
+      return FiatCurrencies.brl;
+    default:
+      return FiatCurrencies.usd;
+  }
+};
