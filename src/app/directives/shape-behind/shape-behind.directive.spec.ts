1214cc75542ea2038f14145da1c5391a8f4f0fae
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-29 17:27:31 -0300

    Creating directive to put shape behind text

---
+++ src/app/directives/shape-behind/shape-behind.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ShapeBehindDirective } from './shape-behind.directive';
+
+describe('ShapeBehindDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ShapeBehindDirective();
+    expect(directive).toBeTruthy();
+  });
+});
