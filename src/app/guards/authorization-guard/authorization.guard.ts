dbd6842fe7d091b36d7ddb9e82fefe2c99bce63a
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-22 00:19:46 -0300

    Handling when authtoken is expired

--- src/app/guards/authorization-guard/authorization.guard.ts
+++ src/app/guards/authorization-guard/authorization.guard.ts
@@ -1,11 +1,14 @@
 import { inject } from '@angular/core';
 import { type CanActivateFn, Router } from '@angular/router';
 import { isNil } from 'lodash-es';
+import { AuthEntity } from 'projects/central-hash-api-client/src/public-api';
+import { firstValueFrom } from 'rxjs';
 import { AuthStateService } from 'src/app/services/auth-state/auth-state.service';
 
-export const authorizationGuard: CanActivateFn = (_route, _state) => {
+export const authorizationGuard: CanActivateFn = async (_route, _state) => {
   const authState = inject(AuthStateService);
   const router = inject(Router);
+  const authEntity = inject(AuthEntity);
   const authToken = authState.get();
   console.log('authToken', authToken);
   if (isNil(authState)) {
@@ -13,8 +16,14 @@ export const authorizationGuard: CanActivateFn = (_route, _state) => {
     return false;
   }
   if (authState.isTokenExpired()) {
-    router.navigate(['/sign-in']);
-    return false;
+    try {
+      const data = await firstValueFrom(authEntity.refresh());
+      authState.set(data);
+      return true;
+    } catch (error) {
+      router.navigate(['/sign-in']);
+      return false;
+    }
   }
   return true;
 };
