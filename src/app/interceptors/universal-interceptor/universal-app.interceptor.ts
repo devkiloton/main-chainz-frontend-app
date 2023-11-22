ed41a75dcdeaf31717d391d6e94da62b0600186d
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-22 00:20:52 -0300

    Adding refresh token when auth token is expired

--- src/app/interceptors/universal-interceptor/universal-app.interceptor.ts
+++ src/app/interceptors/universal-interceptor/universal-app.interceptor.ts
@@ -8,12 +8,20 @@ export class UniversalAppInterceptor implements HttpInterceptor {
   private readonly _authService = core.inject(AuthStateService);
 
   public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const token = this._authService.get();
+    const _token = (): string => {
+      const isValidAuthToken = this._authService.isTokenExpired();
+
+      if (isValidAuthToken) {
+        return this._authService.getRefreshToken() ?? '';
+      }
+      return this._authService.get() ?? '';
+    };
+
     const modifiedReq = request.clone({
       url: request.url,
       setHeaders: {
         // AuthroizationGuard will redirect to /sign-in if token falsy or expired
-        Authorization: `Bearer ${token ?? ''}`,
+        Authorization: `Bearer ${_token()}`,
       },
     });
     return next.handle(modifiedReq);
