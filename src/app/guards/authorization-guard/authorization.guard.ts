12d655f7b5ae6352b6255bf948bce4f5920c9629
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-23 23:45:12 -0300

    Validating if the user is already authenticated

--- src/app/guards/authorization-guard/authorization.guard.ts
+++ src/app/guards/authorization-guard/authorization.guard.ts
@@ -4,13 +4,13 @@ import { isNil } from 'lodash-es';
 import { AuthEntity } from 'projects/central-hash-api-client/src/public-api';
 import { firstValueFrom } from 'rxjs';
 import { AuthStateService } from 'src/app/services/auth-state/auth-state.service';
+import { AuthStoreService } from 'src/app/stores/auth/auth-store.service';
 
 export const authorizationGuard: CanActivateFn = async (_route, _state) => {
   const authState = inject(AuthStateService);
+  const authStore = inject(AuthStoreService);
   const router = inject(Router);
   const authEntity = inject(AuthEntity);
-  const authToken = authState.get();
-  console.log('authToken', authToken);
   if (isNil(authState)) {
     router.navigate(['/sign-in']);
     return false;
@@ -19,11 +19,12 @@ export const authorizationGuard: CanActivateFn = async (_route, _state) => {
     try {
       const data = await firstValueFrom(authEntity.refresh());
       authState.set(data);
-      return true;
     } catch (error) {
       router.navigate(['/sign-in']);
+      authStore.isAuthenticated.set(false);
       return false;
     }
   }
+  authStore.isAuthenticated.set(true);
   return true;
 };
