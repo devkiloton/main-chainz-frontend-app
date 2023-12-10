0d3eaafa7bb320b4d32ffacdfd0d63a2e6c0e820
devkiloton <devkiloton@gmail.com>
2023-12-10 18:33:14 -0300

    Adding features to the dashboard page

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,16 +1,47 @@
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BitcoinService } from 'projects/central-hash-api-client/src/lib/entities/bitcoin/bitcoin.service';
 import { UserEntity } from 'projects/central-hash-api-client/src/public-api';
+import { firstValueFrom } from 'rxjs';
+import { DialogDepositComponent } from 'src/app/shared/dialog-deposit/dialog-deposit.component';
 
 @Component({
   standalone: true,
-  imports: [JsonPipe, AsyncPipe],
+  imports: [
+    JsonPipe,
+    AsyncPipe,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatDialogModule,
+    NgIf,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class DashboardComponent {
   private readonly _userEntity = inject(UserEntity);
-
+  private readonly _dialog = inject(MatDialog);
   public readonly user$ = this._userEntity.find();
+  private readonly _bitcoinService = inject(BitcoinService);
+
+  public readonly walletInformation$ = this._bitcoinService.findInformation();
+
+  public async openDepositDialog(enterAnimationDuration: string, exitAnimationDuration: string): Promise<void> {
+    const publicWallet = await firstValueFrom(this._bitcoinService.findInformation());
+    this._dialog.open(DialogDepositComponent, {
+      enterAnimationDuration,
+      exitAnimationDuration,
+      width: '450px',
+      data: publicWallet,
+    });
+  }
 }
