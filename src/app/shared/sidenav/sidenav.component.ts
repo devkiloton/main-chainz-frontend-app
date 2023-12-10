4c4c69debbe51fb66dcd1f519b9aaf5c09cc642c
devkiloton <devkiloton@gmail.com>
2023-12-10 18:33:42 -0300

    Makign sidenav responsive

--- src/app/shared/sidenav/sidenav.component.ts
+++ src/app/shared/sidenav/sidenav.component.ts
@@ -1,5 +1,9 @@
-import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, ViewChild } from '@angular/core';
 import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
+import { map } from 'rxjs';
+import { AuthStoreService } from 'src/app/stores/auth/auth-store.service';
 import { ButtonChangeCurrencyComponent } from '../button-change-currency/button-change-currency.component';
 import { ButtonChangeThemeComponent } from '../button-change-theme/button-change-theme.component';
 import { ChangeLocaleButtonComponent } from '../change-locale-button/change-locale-button.component';
@@ -14,12 +18,22 @@ import { TreeComponent } from '../tool-bar/components/tree/tree.component';
     ButtonChangeThemeComponent,
     ButtonChangeCurrencyComponent,
     ChangeLocaleButtonComponent,
+    AsyncPipe,
+    NgIf,
   ],
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
   changeDetection: ChangeDetectionStrategy.Default,
 })
 export class SidenavComponent {
+  private readonly _authEntity = inject(AuthStoreService);
+  private readonly _breakpointObserver = inject(BreakpointObserver);
+
+  public readonly isAuthenticated = this._authEntity.isAuthenticated;
+  public readonly isDesktop$ = this._breakpointObserver
+    .observe('(max-width: 1024px)')
+    .pipe(map(value => !value.matches));
+
   @ViewChild(MatDrawer)
   public readonly drawer!: MatDrawer;
 
